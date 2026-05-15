c8f62410e91a14e7c4a17207444fdbb1b27940ea
Hidar022 <aliyubinahmad2020@gmail.com>
2026-05-15 09:46:17 +0100

    confiiguration of portfolio

--- bin_portfolio/settings.py
+++ bin_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9v7n4-)+ek+l%)b1r^e_l5k3*r0_vhg-#es5ha53aw%!^rnl*!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bin_portfolio.vercel.app']
 
 
 # Application definition
